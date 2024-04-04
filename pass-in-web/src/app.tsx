75190d9de41e5b36ead955acd769e0ec70bf70bb
Isabele Rocha <isabeledpr@gmail.com>
2024-04-04 13:04:35 -0300

    aula 1

---
+++ pass-in-web/src/app.tsx
@@ -0,0 +1,12 @@
+import { AttendeeList } from "./components/attendee-list";
+import { Header } from "./components/header";
+
+export function App() {
+
+  return (
+    <div>
+      <Header />
+      <AttendeeList />
+    </div>
+  )
+}
